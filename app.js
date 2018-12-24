e941924d26396b6f7d3caf0a948ab1c778b1f170
André Antunes <andrepalhares1989@gmail.com>
2018-12-24 00:08:03 -0200

    Calculate & Error

---
+++ app.js
@@ -0,0 +1,57 @@
+// Listen for submit
+document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  
+  //  UI Vars
+  const amount = document.querySelector('#amount');
+  const interest = document.querySelector('#interest');
+  const years = document.querySelector('#years');
+  const monthlyPayment = document.querySelector('#monthly-payment');
+  const totalPayment = document.querySelector('#total-payment');
+  const totalInterest = document.querySelector('#total-interest');
+
+  // Calculations
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) /100 /12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+  const monthly = (principal * x * calculatedInterest) / (x-1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    showError('Please check your numbers');
+  }
+  e.preventDefault();
+}
+
+function showError(error) {
+  // Create Error div
+  const errorDiv = document.createElement('div');
+
+  // Get elements (to insert the Error div)
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class to div
+  errorDiv.className = 'alert alert-danger';
+
+  // Add a text to the Error div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert Error div
+  card.insertBefore(errorDiv, heading);
+
+  // Set time out
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
